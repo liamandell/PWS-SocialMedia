f3a861383882b622f27feef91cb8231ec060654a
liamandell <liamandell14@gmail.com>
2024-02-07 18:27:48 -0600

    update

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const {
+    
+Schema,model
+} = require('mongoose');
+const reactionSchema = require('./Reaction');
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [
+          reactionSchema
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    });
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+const Thought = model('Thought', thoughtSchema);
+module.exports = Thought;
